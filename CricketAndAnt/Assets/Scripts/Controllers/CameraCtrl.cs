7b3aa381d119746856d527ca4d14ca0ad796e426
NastyTester <70013669+AlinNast@users.noreply.github.com>
2023-01-15 16:41:18 +0200

    added text plaques

--- CricketAndAnt/Assets/Scripts/Controllers/CameraCtrl.cs
+++ CricketAndAnt/Assets/Scripts/Controllers/CameraCtrl.cs
@@ -8,23 +8,27 @@ using UnityEngine;
 public class CameraCtrl : MonoBehaviour
 {
     public GameObject player;
+    SpriteRenderer playerSR;
 
     public Transform backGround;
 
    
     public float Yoffset;
     public float Xoffset;
+    public float smoothOffset;
 
     // Start is called before the first frame update
     void Start()
     {
         transform.position = new Vector3(player.transform.position.x + Xoffset, player.transform.position.y + Yoffset, -10);
         backGround.position = new Vector3(player.transform.position.x + Xoffset, player.transform.position.y + Yoffset, 0);
+        playerSR = player.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 tempPos = new Vector3(player.transform.position.x, player.transform.position.y, -10);
         float maxFall = -10;
         if (player.transform.position.y < maxFall)
         {
@@ -32,7 +36,15 @@ public class CameraCtrl : MonoBehaviour
         }
         else
         {
-            transform.position = new Vector3(player.transform.position.x + Xoffset, player.transform.position.y + Yoffset, -10);
+            if (playerSR.flipX)
+            {
+                tempPos = new Vector3(player.transform.position.x + Xoffset, player.transform.position.y + Yoffset, -10);
+            }
+            else
+            {
+                tempPos = new Vector3(player.transform.position.x - Xoffset, player.transform.position.y + Yoffset, -10);
+            }
+            transform.position = Vector3.Lerp(transform.position, tempPos, smoothOffset * Time.deltaTime);
         }
     }
 }
--- CricketAndAnt/Assets/Scripts/Controllers/TextTriggerCtrl.cs
+++ CricketAndAnt/Assets/Scripts/Controllers/TextTriggerCtrl.cs
@@ -1,33 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 
 /// <summary>
 /// makes text messeges ingame, works by hiding the sprites on the z axis
 /// </summary>
 public class TextTriggerCtrl : MonoBehaviour
 {
-    public Transform textBaner;
-    public Transform textMessege;
+    public GameObject background;
+    public Text text;
+
+    private SpriteRenderer backgroundSR;
     // Start is called before the first frame update
     void Start()
     {
-        textBaner.transform.position = new Vector3(textBaner.transform.position.x, textBaner.transform.position.y, -50);
-        textMessege.transform.position = new Vector3(textMessege.transform.position.x, textMessege.transform.position.y, -50);
+        backgroundSR = background.GetComponent<SpriteRenderer>();
+        backgroundSR.color = new Color(1, 1, 1, 0);
+        text.color = new Color(1, 1, 1, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        
-        textBaner.transform.position = new Vector3(textBaner.transform.position.x, textBaner.transform.position.y, 1);
-        textMessege.transform.position = new Vector3(textMessege.transform.position.x, textMessege.transform.position.y, 0);
+        backgroundSR.color = new Color(0.09433961f, 0.0467248f, 0.0467248f, 0.5f);
+        text.color = new Color(1, 1, 1, 1);
+        Debug.Log("enter plaque");
 
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        text.color = new Color(1, 1, 1, 0);
+        backgroundSR.color = new Color(1, 1, 1, 0);
+        Debug.Log("exit plaque");
     }
 }
